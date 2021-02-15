8971240cbfb64379cbc3e885ebd32b67231da49c
devanshukushwah <61180182+devanshukushwah@users.noreply.github.com>
2021-02-15 21:10:49 +0530

    15feb

--- popup.js
+++ popup.js
@@ -15,183 +15,184 @@ var n_o = -1;
 console.log("you in popup.js file!");
 
 
-$(function(){
+$(function () {
 
     ser_chat();
 
     $('#data').val(word);
 
 
-    $('#chat').keyup(function chat(e){
+    $('#chat').keyup(function chat(e) {
         // e.preventDefault();
         // $('#chat').val("done");
         var temp_chat = $('#chat').val();
         firebase.database().ref('chat/D&A').set({
-            chat:temp_chat
+            chat: temp_chat
         });
         // ser_chat();
     });
 
-    function ser_chat(){
+    function ser_chat() {
         // textAreaAdjust();
-        firebase.database().ref('chat/D&A').on('value', function(snapshot){
- 
+        firebase.database().ref('chat/D&A').on('value', function (snapshot) {
+
             const ta = snapshot.val();
             $('#chat').val(ta.chat);
 
-  });
+        });
     }
-    $('#codebtn').click(function code(e){
-        $("#jsloader").css("display","block");
+    $('#codebtn').click(function code(e) {
+        $("#jsloader").css("display", "block");
         $("section").empty();
-        var txt1 = "<iframe height=\"400px\" width=\"600px\" src=\"https://repl.it/@DevanshuKushwah/c?lite=true\" scrolling=\"no\" frameborder=\"no\" allowtransparency=\"true\" allowfullscreen=\"true\" sandbox=\"allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals\"></iframe>"; 
+        var txt1 = "<iframe height=\"400px\" width=\"600px\" src=\"https://repl.it/@DevanshuKushwah/c?lite=true\" scrolling=\"no\" frameborder=\"no\" allowtransparency=\"true\" allowfullscreen=\"true\" sandbox=\"allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals\"></iframe>";
         $("body").append(txt1);
-        setTimeout(function(){ $("#jsloader").css("display","none"); }, 5000);
+        setTimeout(function () {
+            $("#jsloader").css("display", "none");
+        }, 5000);
 
     });
     // function 
 
 });
 
-var tame,res,res2,tpe;
+var tame, res, res2, tpe;
 var firebaseConfig = {
-   apiKey: "AIzaSyB2rIszVzmYFDn0ceSxNF59nCxyVn4u83A",
-   authDomain: "notepad-511d6.firebaseapp.com",
-   projectId: "notepad-511d6",
-   storageBucket: "notepad-511d6.appspot.com",
-   messagingSenderId: "226781764245",
-   appId: "1:226781764245:web:65521e6b87cc292f2e599c",
-   measurementId: "G-SRY0LN8K3J"
- };
- // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
- // firebase.analytics();
+    apiKey: "AIzaSyB2rIszVzmYFDn0ceSxNF59nCxyVn4u83A",
+    authDomain: "notepad-511d6.firebaseapp.com",
+    projectId: "notepad-511d6",
+    storageBucket: "notepad-511d6.appspot.com",
+    messagingSenderId: "226781764245",
+    appId: "1:226781764245:web:65521e6b87cc292f2e599c",
+    measurementId: "G-SRY0LN8K3J"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
- var topic,data;
- function Ready(){
-	    topic = $('#topic').val().trim().toLowerCase();
-        data = $('#data').val().trim();
-        tame = Date.now();
-        res = data.slice(0, 7);
-        res2 = data.slice(0, 8);
-        ftpe();
+var topic, data;
 
- }
- function ftpe(){
+function Ready() {
+    topic = $('#topic').val().trim().toLowerCase();
+    data = $('#data').val().trim();
+    tame = Date.now();
+    res = data.slice(0, 7);
+    res2 = data.slice(0, 8);
+    ftpe();
+
+}
+
+function ftpe() {
     topic = $('#topic').val().trim().toLowerCase();
     data = $('#data').val().trim();
     // tame = Date.now();
     res = data.slice(0, 7);
     res2 = data.slice(0, 8);
-    if( res == "http://" || res2 == "https://"){
-    tpe = "link";
- }
- else{
-    tpe = "description";
- }
+    if (res == "http://" || res2 == "https://") {
+        tpe = "link";
+    } else {
+        tpe = "description";
+    }
 }
 
-$(function(e){
-    $('form').submit(function funsubmit(e){
+$(function (e) {
+    $('form').submit(function funsubmit(e) {
         // $('#status').html("Wait For A Second");
         // $('#status1').css("background-color","white");
         // $('#status2').css("background-color","#E60000");
         // $('#status3').css("background-color","white");
         Ready();
         e.preventDefault();
-               if(topic!="" && data != ""){//.....................................................// if start
-                $('#status1').css("background-color","white");
-                $('#status2').css("background-color","#E60000");
-                $('#ste').html("Wait!");
-                $('#ste').css("color","#E60000");
+        if (topic != "" && data != "") { //.....................................................// if start
+            $('#status1').css("background-color", "white");
+            $('#status2').css("background-color", "#E60000");
+            $('#ste').html("Wait!");
+            $('#ste').css("color", "#E60000");
 
-                $('#status3').css("background-color","white");
-                // if( res == "http://" || res2 == "https://"){
-               firebase.database().ref('notepad/' + topic + "/" + tame).set({
-                topic:topic,
-                data:data,
-                type:tpe,
-                time:tame
-            }).then(() => {//.............................//server request verification start
+            $('#status3').css("background-color", "white");
+            // if( res == "http://" || res2 == "https://"){
+            firebase.database().ref('notepad/' + topic + "/" + tame).set({
+                topic: topic,
+                data: data,
+                type: tpe,
+                time: tame
+            }).then(() => { //.............................//server request verification start
                 // $('#status').html("Well! Done");
-        $('#status1').css("background-color","white");
-        $('#status2').css("background-color","white");
-        $('#ste').html("Well Done!");
-                $('#ste').css("color","#FF7800");
-                $('#status3').css("background-color","#FF7800");
-                        $('#topic').focus();
-                        $('#topic').val('');
-                        $('#data').val('');
-            });//.........................................//server request verification end
-        // }
-        // else{
-        //     firebase.database().ref('notepad/' + topic + "/" + tame).set({
-        //         topic:topic,
-        //         data:data,
-        //         type:"description",
-        //         time:tame
-        //     }).then(() => {//.............................//server request verification start
-        //         $('#status1').css("background-color","white");
-        //         $('#status2').css("background-color","white");
-        //         $('#ste').html("Well Done!");
-        //         $('#ste').css("color","#FF7800");
-        //                 $('#status3').css("background-color","#FF7800");
-        //                         $('#topic').focus();
-        //                         $('#topic').val('');
-        //                         $('#data').val('');
-        //     });//.........................................//server request verification end
-        // }
+                $('#status1').css("background-color", "white");
+                $('#status2').css("background-color", "white");
+                $('#ste').html("Well Done!");
+                $('#ste').css("color", "#FF7800");
+                $('#status3').css("background-color", "#FF7800");
+                $('#topic').focus();
+                $('#topic').val('');
+                $('#data').val('');
+            }); //.........................................//server request verification end
+            // }
+            // else{
+            //     firebase.database().ref('notepad/' + topic + "/" + tame).set({
+            //         topic:topic,
+            //         data:data,
+            //         type:"description",
+            //         time:tame
+            //     }).then(() => {//.............................//server request verification start
+            //         $('#status1').css("background-color","white");
+            //         $('#status2').css("background-color","white");
+            //         $('#ste').html("Well Done!");
+            //         $('#ste').css("color","#FF7800");
+            //                 $('#status3').css("background-color","#FF7800");
+            //                         $('#topic').focus();
+            //                         $('#topic').val('');
+            //                         $('#data').val('');
+            //     });//.........................................//server request verification end
+            // }
 
 
 
-        }//....................................................................................... // if end
-        else{
+        } //....................................................................................... // if end
+        else {
             // $('#status').html("Fill Both Input!");
-            if(!$('#topic').val()){
-                $('#topic').attr("placeholder","Fill this block!")
+            if (!$('#topic').val()) {
+                $('#topic').attr("placeholder", "Fill this block!")
+            } else {
+                $('#data').attr("placeholder", "Fill this block!")
             }
-            else{
-                $('#data').attr("placeholder","Fill this block!")
-            }
-        } 
+        }
     })
 
 
-    $('#topic').change(function chnge(e){
+    $('#topic').change(function chnge(e) {
         e.preventDefault();
         n_o = -1;
     })
 
-    $('#schbtn').click(function sch(e){
+    $('#schbtn').click(function sch(e) {
         $('#ste').html("Wait!");
 
-        n_o+=1;
-        if($('#topic').val()==""){
+        n_o += 1;
+        if ($('#topic').val() == "") {
             console.log("nothing");
             $('#ste').html("Type Topic To Search!");
-            $('#ste').css("color","#E60000");
+            $('#ste').css("color", "#E60000");
 
             return true;
         }
         e.preventDefault();
         Ready();
         console.log("click on serach!");
-        firebase.database().ref('notepad/' + topic).orderByChild('data').on('value', function(snapshot){
-            if(!snapshot.exists()){
+        firebase.database().ref('notepad/' + topic).orderByChild('data').on('value', function (snapshot) {
+            if (!snapshot.exists()) {
                 console.log("not exist");
                 $('#ste').html("Not Found!");
-                $('#ste').css("color","#E60000");
+                $('#ste').css("color", "#E60000");
 
                 return true;
             }
             var te = snapshot.val();
             // console.log(Object.keys(te).length-2);
-            if(n_o >= parseInt(Object.keys(te).length-1)){
+            if (n_o >= parseInt(Object.keys(te).length - 1)) {
                 $('#ste').html("This is Last Data!");
-                n_o = Object.keys(te).length-1;
-            }
-            else{
-            $('#ste').html("Click Again For Next!");
+                n_o = Object.keys(te).length - 1;
+            } else {
+                $('#ste').html("Click Again For Next!");
 
             }
             // $('#ste').css("background-color","#FF7800");
@@ -204,19 +205,19 @@ $(function(e){
             topic = te[infoData].topic;
             // console.log(data);
             $('#data').val(data);
-    
+
 
         });
 
     });
 
-    $('#updbtn').click(function upd(e){
+    $('#updbtn').click(function upd(e) {
         $('#ste').html("Wait!");
         // n_o+=1;
-        if($('#topic').val()==""){
+        if ($('#topic').val() == "") {
             console.log("nothing");
             $('#ste').html("Type Topic To Update!");
-            $('#ste').css("color","#E60000");
+            $('#ste').css("color", "#E60000");
 
             return true;
         }
@@ -226,24 +227,24 @@ $(function(e){
         console.log("click on update!");
         data = $('#data').val();
         firebase.database().ref('notepad/' + topic + "/" + tame).set({
-            topic:topic,
-            data:data,
-            type:tpe,
-            time:tame
-        }).then(() => {//.............................//server request verification start
+            topic: topic,
+            data: data,
+            type: tpe,
+            time: tame
+        }).then(() => { //.............................//server request verification start
 
             $('#ste').html("Updated!");
-            $('#ste').css("color","#FF7800");
-        });//.........................................//server request verification end
+            $('#ste').css("color", "#FF7800");
+        }); //.........................................//server request verification end
     });
 
-    $('#delbtn').click(function del(e){
+    $('#delbtn').click(function del(e) {
         $('#ste').html("Wait!");
         // n_o+=1;
-        if($('#topic').val()==""){
+        if ($('#topic').val() == "") {
             console.log("nothing");
             $('#ste').html("Type Topic To Delete!");
-            $('#ste').css("color","#E60000");
+            $('#ste').css("color", "#E60000");
 
             return true;
         }
@@ -252,14 +253,14 @@ $(function(e){
         console.log("click on delete!");
         // data = $('#data').val();
         $('#data').val("");
-        firebase.database().ref('notepad/' + topic + "/" + tame).remove().then(() => {//.............................//server request verification start
+        firebase.database().ref('notepad/' + topic + "/" + tame).remove().then(() => { //.............................//server request verification start
 
             $('#ste').html("Deleted!");
-            $('#ste').css("color","#FF7800");
+            $('#ste').css("color", "#FF7800");
             // n_o-=2;
             // sch();
 
-        });//.........................................//server request verification end
+        }); //.........................................//server request verification end
     });
 
 
